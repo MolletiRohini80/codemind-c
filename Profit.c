fa7aab84206065647af0b73f257220d0829afaa9
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2022-12-06 09:29:51 +0530

    #include<stdio.h> int main() {     int pr,l;     scanf("%d%d",&pr,&l);     if(l>pr)     {         printf("Profit");     }     else     {         printf("Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int pr,l;
+    scanf("%d%d",&pr,&l);
+    if(l>pr)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+}
