87249ef1cdef86c281c2339e64c1fac8c203eb42
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2022-12-28 20:49:54 +0530

    #include<stdio.h> int main() {     int n,c;     float sr,b;     scanf("%d",&n);     if(n<=199)     c=1.20*n;     else if(200<n&&n<400)     c=1.50*n;     else if(n>400&&n<600)     c=1.80*n;     else if(n>=600)     c=2.00*n;     if(c>400)     sr=.15*c;     else     sr=100;     b=c+sr;     printf("%.2f",b);
    }

--- Current_bill.c
+++ Current_bill.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int u,c;
-    scanf("%d",&u);
-    if(u<=199)
-    c=1.20*u;
-    else if(u>200&&u<400)
-    c=1.50*u;
-    else if(u>400&&u<600)
-     c=1.80*u;
-    else if(u>=600)
-    c=2*u;
-    float sr;
+    int n,c;
+    float sr,b;
+    scanf("%d",&n);
+    if(n<=199)
+    c=1.20*n;
+    else if(200<n&&n<400)
+    c=1.50*n;
+    else if(n>400&&n<600)
+    c=1.80*n;
+    else if(n>=600)
+    c=2.00*n;
     if(c>400)
-      sr=.15*c;
-    else
-      sr=100;
-    float b;
+    sr=.15*c;
+    else 
+    sr=100;
     b=c+sr;
     printf("%.2f",b);
+    
 }
