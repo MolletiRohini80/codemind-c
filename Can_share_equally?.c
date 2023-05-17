c72337289caeffe16ac6c03c0d0d338c8142adb4
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-17 20:05:58 +0530

    #include<stdio.h> int main() {     int a,b,r=0;     scanf("%d%d",&a,&b);     r=a-2*b;     if (a==0 && b%2==0)     {     printf("YES");     }     else if (a==0 && b%2!=0)     {     printf("NO");     }     else if (r%2==0)     {     printf("YES");     }     else     {     printf("NO");     } }

--- Can_share_equally?.c
+++ Can_share_equally?.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int x,y;
-    scanf("%d%d",&x,&y);
-    if(x!=0&&x%2==0 || x==0&&y%2==0)
+    int a,b,r=0;
+    scanf("%d%d",&a,&b);
+    r=a-2*b;
+    if (a==0 && b%2==0)
     {
-        printf("YES");
+    printf("YES");
+    }
+    else if (a==0 && b%2!=0)
+    {
+    printf("NO");
+    }
+    else if (r%2==0)
+    {
+    printf("YES");
     }
     else
     {
-        printf("NO");
+    printf("NO");
     }
 }
