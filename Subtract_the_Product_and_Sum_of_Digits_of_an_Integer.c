2fb2891ef3d1b86adc2295904131413c7b0d85ce
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-18 10:30:10 +0530

    #include<stdio.h> int main() {     int n,s=0,i,p=1,x;     scanf("%d",&n);     for(;n>0;)     {         i=n%10;         s=s+i;         p=p*i;         n=n/10;     }     x=p-s;     printf("%d",x); }

--- Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,p=1,l,x;
+    int n,s=0,i,p=1,x;
     scanf("%d",&n);
     for(;n>0;)
     {
-        l=n%10;
-        s=s+l;
-        p=p*l;
+        i=n%10;
+        s=s+i;
+        p=p*i;
         n=n/10;
     }
     x=p-s;
