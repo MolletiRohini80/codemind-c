c898890e098de60c5aa8d07a07e145bf9bc99ca2
MolletiRohini80 <119840376+MolletiRohini80@users.noreply.github.com>
2023-05-26 21:21:13 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     float f;     f=((n-32)*5.0)/9.0;     printf("%0.2f",f); }

--- Fahrenheit_to_Celsius.c
+++ Fahrenheit_to_Celsius.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    float f,c;
-    scanf("%f",&f);
-    c=(f-32)/1.8;
-    printf("%.2f",c);
+    int n;
+    scanf("%d",&n);
+    float f;
+    f=((n-32)*5.0)/9.0;
+    printf("%0.2f",f);
 }
